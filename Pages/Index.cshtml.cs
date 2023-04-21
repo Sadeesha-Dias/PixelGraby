77a514015b3664fed30de39c4f2cd997cd3b922f
Sadeesha-Dias <sddias19981230@gmail.com>
2023-04-21 19:46:40 +0530

    Added the backend code to download the video file and store in a given folder.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Net.Http.Headers;
 
 namespace PixelGraby.Pages
 {
@@ -12,9 +14,29 @@ namespace PixelGraby.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnPost(string url)
         {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("video/mp4"));
 
+                using (var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    var fileName = Path.GetFileName(url);
+                    var filePath = Path.Combine(@"C:\Users\User\OneDrive\Desktop\TestV", fileName);
+
+                    using (var stream = await response.Content.ReadAsStreamAsync())
+                    {
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await stream.CopyToAsync(fileStream);
+                        }
+                    }
+                }
+            }
         }
     }
 }
